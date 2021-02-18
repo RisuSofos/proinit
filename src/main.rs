a511a152627d98ceaeb3333acf9f28800403d446
RisuSofos <risusofos@gmail.com>
2021-02-17 21:59:33 -0600

    added basic logic for parsing commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = mk_app().get_matches();
+}
+
+fn mk_app() -> App<'static, 'static> {
+    let app = App::new("proinit")
+        .version("0.1.0")
+        .author("RisuSofos <risusofos@gmail.com>")
+        .about("Initializes project workspaces for various project types")
+        .arg(Arg::with_name("Web Page")
+            .long("web-page")
+            .value_name("output directory")
+            .help("Initializes a webpage project")
+            .takes_value(true)
+        )
+        .usage("proinit [options] <output_file>");
+    app
 }
